0342952369015db352e399803bb2729e84fc75a7
ke46138 <ke46138+gpg@gmail.com>
2025-06-06 13:57:39 +0300

    Смена алгоритма авторизации

--- modules/restrict.py
+++ modules/restrict.py
@@ -1,6 +1,7 @@
 from modules import sqlite_adapter as sq
+from modules import auth
 from modules import botdebug as d
-from config import admins, allowed_chats, user_blacklist
+import config
 import time
 import traceback
 
@@ -9,8 +10,6 @@ def setup_handlers(bot):
     @bot.message_handler(content_types=["new_chat_members"])
     def new_member(message):
         try:
-            time.sleep(1)
-
             for new_member in message.new_chat_members:
                 user_id = new_member.id
                 sq.add_user(user_id, message.chat.id)
@@ -19,10 +18,8 @@ def setup_handlers(bot):
                     can_send_messages=True,
                     can_send_media_messages=True,
                     can_send_other_messages=True,
-                    can_add_web_page_previews=True)            
-                time.sleep(1)
-
-        except Exception as e:
+                    can_add_web_page_previews=True)
+        except:
             d.send_view_traceback(message, traceback.format_exc())
 
     @bot.message_handler(content_types=['left_chat_member'])
@@ -33,52 +30,54 @@ def setup_handlers(bot):
     @bot.message_handler(commands=['only_admins'])
     def only_admins(message):
         try:
-            if message.sender_chat:
-                user_id = message.sender_chat.id
-            elif message.from_user:
-                user_id = message.from_user.id
-            else:
-                bot.reply_to(message, "Ошибка: не удалось определить отправителя. ПОЗДРАВЛЯЮ, КАК ТЫ ЭТО СДЕЛАЛ?")
-                return
+            result = auth.authorize(message)
 
-            if user_id not in admins and user_id not in allowed_chats:
+            if result == 0:
                 bot.reply_to(message, "У вас нет прав на использование этой команды")
                 return
+            elif result == -1:
+                bot.reply_to(message, "Ошибка: не удалось определить отправителя. ПОЗДРАВЛЯЮ, КАК ТЫ ЭТО СДЕЛАЛ?")
+                return
+            elif result == 1:
+                pass
+            else:
+                raise ValueError("Итак вопрос: математика сломалась?")
 
             result = sq.select_all_where('users_table', message.chat.id)
             muted_users_raw = sq.select_column_where('mute_table', 'id', message.chat.id)
             muted_users = [item[0] for item in muted_users_raw]
 
             for i in range(len(result)):
-                if result[i][0] not in admins and result[i][0] not in allowed_chats and result[i][0] not in muted_users and result[i][0] not in user_blacklist:
+                if result[i][0] not in config.admins and result[i][0] not in config.allowed_chats and result[i][0] not in muted_users and result[i][0] not in config.user_blacklist:
                     bot.restrict_chat_member(message.chat.id, result[i][0], can_send_messages=False)
                 time.sleep(0.1)
             
             bot.reply_to(message, 'Теперь писать могут только админы')
-        except Exception as e:
+        except:
             d.send_view_traceback(message, traceback.format_exc())
 
     @bot.message_handler(commands=['all_users'])
     def all_users(message):
         try:
-            if message.sender_chat:
-                user_id = message.sender_chat.id
-            elif message.from_user:
-                user_id = message.from_user.id
-            else:
-                bot.reply_to(message, "Ошибка: не удалось определить отправителя. ПОЗДРАВЛЯЮ, КАК ТЫ ЭТО СДЕЛАЛ?")
-                return
+            result = auth.authorize(message)
 
-            if user_id not in admins and user_id not in allowed_chats:
+            if result == 0:
                 bot.reply_to(message, "У вас нет прав на использование этой команды")
                 return
+            elif result == -1:
+                bot.reply_to(message, "Ошибка: не удалось определить отправителя. ПОЗДРАВЛЯЮ, КАК ТЫ ЭТО СДЕЛАЛ?")
+                return
+            elif result == 1:
+                pass
+            else:
+                raise ValueError("Итак вопрос: математика сломалась?")
             
             result = sq.select_all_where('users_table', message.chat.id)
             muted_users_raw = sq.select_column_where('mute_table', 'id', message.chat.id)
             muted_users = [item[0] for item in muted_users_raw]
 
             for i in range(len(result)):
-                if result[i][0] not in admins and result[i][0] not in allowed_chats and result[i][0] not in muted_users:
+                if result[i][0] not in config.admins and result[i][0] not in config.allowed_chats and result[i][0] not in muted_users and result[i][0] not in config.user_blacklist:
                     bot.restrict_chat_member(
                             message.chat.id, result[i][0],
                             can_send_messages=True,
@@ -89,5 +88,5 @@ def setup_handlers(bot):
 
             bot.reply_to(message, 'Теперь писать могут все, кроме замученных пользователей')
 
-        except Exception as e:
+        except:
             d.send_view_traceback(message, traceback.format_exc())
